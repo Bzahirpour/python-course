3fcbdb47324c500a24008d4762c12f5727911143
Benjamin Zahirpour <benjamin@zahirpour.com>
2024-04-14 09:11:20 -0400

    changed file name from first program to print, added new file variables

---
+++ print.py
@@ -0,0 +1,9 @@
+# First print
+print('Benjamin Zahirpour')
+
+# Print a dog, code is proccessed from top to bottom
+print('o----')
+print(' ||||')
+
+# Print * multiplied by 10
+print('*' * 10)
---
+++ variables.py
@@ -0,0 +1,13 @@
+# Assign 10 (intger) to the variable price, and print it to the screen
+price = 10
+print(price)
+
+# Assign a float, a string, and a boolean (case sensitive)
+rating = 4.9
+name = 'Ben'
+is_published = False
+
+# Challange: define three variables for a hospital patient John Smith, 20 years old, new patient
+name = 'John Smith'
+age = 20
+new_patient = True
