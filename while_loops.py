1743a7e6470e339a80565b3c1e2c05f04846b649
Benjamin Zahirpour <156618785+Bzahirpour@users.noreply.github.com>
2024-04-21 21:21:16 -0400

    added while loops

---
+++ while_loops.py
@@ -0,0 +1,13 @@
+# Done wont be printed until the condition for the loop is false
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
+print('Done')
+
+#
+i = 1
+while i <= 5:
+    print('*' * i)
+    i = i + 1
+
