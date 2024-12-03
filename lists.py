713e035d5e52b5490c3f1989dc5cda86d2b84f8d
Bzahirpour <benjaminzfitness@gmail.com>
2024-12-02 19:52:59 -0500

    update

--- lists.py
+++ lists.py
@@ -4,3 +4,26 @@ for current_number in list:
     if biggest_number < current_number:
         biggest_number = current_number
 print(biggest_number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list = [1,2,9,6,8,7,3,4,5,6,8]
+biggest_number = list[0]
+for current_number in list:
+    if biggest_number < current_number:
+        biggest_number = current_number
+print(biggest_number)
--- nested_loops.py
+++ nested_loops.py
@@ -8,3 +8,24 @@ for current_number in list:
     for y in range(current_number):
         output = output + 'x'
     print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list = [1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1]
+for current_number in list:
+    output = ''
+    for y in range(current_number):
+        output += 'x'
+    print(output)
