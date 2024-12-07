80d532fb39822cf3ff11d4462657ae1ab37c3954
Bzahirpour <benjaminzfitness@gmail.com>
2024-12-07 09:56:42 -0500

    added dictionary

---
+++ dictionary.py
@@ -0,0 +1,40 @@
+device_info = {
+    'hostname': 'USVA3_VA3_301_312_U35_1-3-2',
+    'model': 'NTK665AA',
+    'sn': 'Ciena-OTN-1234-ABCD-5678-XYZ9',
+    'location': 'US East, Coresite VA3, Room 301'
+}
+device_info_request = ''
+while device_info_request != 'quit':
+    device_info_request = input('device info: ')
+    if device_info_request == 'help':
+        print(
+'''
+hostname
+model
+sn
+location
+quit
+'''
+        )
+    elif device_info_request == 'quit':
+        exit()
+    else:
+        print(device_info.get(device_info_request, 'does not exist in device info, type help for options'))
+
+
+
+
+
+
+
+
+
+device_info = {
+    'hostname': 'USVA3_VA3_301_312_U35_1-3-2',
+    'model': 'NTK665AA',
+    'sn': 'Ciena-OTN-1234-ABCD-5678-XYZ9',
+    'location': 'US East, Coresite VA3, Room 301'
+}
+device_info_request = input('device info: ')
+print(device_info.get(device_info_request))
