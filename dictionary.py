d4e1a26b41fb29df45960616f2e30970d74fc356
Bzahirpour <benjaminzfitness@gmail.com>
2024-12-07 19:02:45 -0500

    added new files

--- dictionary.py
+++ dictionary.py
@@ -5,36 +5,18 @@ device_info = {
     'location': 'US East, Coresite VA3, Room 301'
 }
 device_info_request = ''
-while device_info_request != 'quit':
-    device_info_request = input('device info: ')
-    if device_info_request.lower() == 'help':
-        print(
-'''
+while True:
+    device_info_request = input('device info: ').lower()
+    if device_info_request == 'help':
+        print('''
+OPTIONS:
 hostname
 model
 sn
 location
 quit
-'''
-        )
-    elif device_info_request.lower() == 'quit':
+''')
+    elif device_info_request == 'quit':
         exit()
     else:
-        print(device_info.get(device_info_request.lower(), 'does not exist in device info, type help for options'))
-
-
-
-
-
-
-
-
-
-device_info = {
-    'hostname': 'USVA3_VA3_301_312_U35_1-3-2',
-    'model': 'NTK665AA',
-    'sn': 'Ciena-OTN-1234-ABCD-5678-XYZ9',
-    'location': 'US East, Coresite VA3, Room 301'
-}
-device_info_request = input('device info: ')
-print(device_info.get(device_info_request))
+        print(device_info.get(device_info_request, 'does not exist in device info, type help for options'))
---
+++ functions.py
@@ -0,0 +1,23 @@
+def greet_user(name):
+    print(f'Hi, {name}!')
+    print('Welcome aboard')
+
+name = input('What is your name? ').capitalize()
+print('Start')
+greet_user(name)
+print('Finish')
+
+
+
+#names in this case are the parameter (the placeholders for our arguments)
+def greet_names(first_name, last_name):
+    print(f'Hello, {first_name} {last_name}')
+    print('How are you today?')
+
+#john and mary are the arguments (the things that get assigned to our parameter)
+greet_names('John', 'Smith')
+greet_names('Mary', 'Hatton')
+
+
+greet_names(last_name='John', first_name='Smith')
+greet_names('Mary', 'Hatton')
