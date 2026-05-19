b3c6169c13450ad15e2856dd1b5aa0a13ac667f5
Bzahirpour <Bzahirpour@users.noreply.github.com>
2026-05-19 15:47:56 -0400

    added packages

--- classes.py
+++ classes.py
@@ -1,12 +1,12 @@
 class Point: #classes use PascalNamingConvneion
-    #defining methods
+    #defining methods in the body of the class
     def move(self):
         print('move')
 
     def draw(self):
         print ('draw')
 
-
+# attributes can be set anywhere in the program
 point1 = Point()
 point1.x = 10
 point1.y = 20
---
+++ utils.py
@@ -0,0 +1,7 @@
+def biggest_number(numbers):
+    biggest = numbers[0]
+    for number in numbers:
+        if number > biggest:
+            biggest = number
+    return biggest
+
